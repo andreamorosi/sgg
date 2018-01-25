d38121a6cab2e56f8c3070ea5292b391853a39e8
andreamorosi <andreamorosi94@gmail.com>
2018-01-26 00:23:03 +0100

    Animatori page, relative media and some fix here and there

---
+++ wp-content/themes/understrap/animatori.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Template Name: Animatori
+ *
+ * @package understrap
+ */
+
+get_header();
+?>
+
+<!-- Wrapper Hero -->
+<section class="wrapper hero-wrapper" id="wrapper-hero">
+    <figure class="hero-banner"></figure>
+</section><!-- //Wrapper Hero -->
+
+<!-- Wrapper Animatori -->
+<section class="wrapper animatori-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="wrapper-title">
+                    <span class="pre-titolo f-bold">Animatori</span>
+                    <h3 class="verde pacifico">Scopriamo i volti</h3>
+                </div>
+            </div>
+
+            <div class="col-sm-12 offset-sm-0 col-md-6 offset-md-3 text-center">
+                <p class="margin-v-2">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam voluptate iusto nam dolore corporis voluptatibus tempore, labore incidunt! Ipsa eum debitis facere hic inventore, labore reiciendis excepturi quo tempora nesciunt.
+                </p>
+            </div>
+
+            <div class="col-12 animatori-inner">
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Alberto_1.png">
+                    <span class="pacifico">Alberto</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Alessio_1.png">
+                    <span class="pacifico">Alessio</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Alice_1.png">
+                    <span class="pacifico">Alice</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Amanda_1.png">
+                    <span class="pacifico">Amanda</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Antr_1.png">
+                    <span class="pacifico">Antr</span>
+                </figure>
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Bova_1.png">
+                    <span class="pacifico">Bova</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Camilla_1.png">
+                    <span class="pacifico">Camilla</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Davide_1.png">
+                    <span class="pacifico">Davide</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Elena_1.png">
+                    <span class="pacifico">Elena</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Francesca_1.png">
+                    <span class="pacifico">Francesca</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Giacomo_1.png">
+                    <span class="pacifico">Giacomo</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Giulia_1.png">
+                    <span class="pacifico">Giulia</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Jonny_1.png">
+                    <span class="pacifico">Jonny</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Luca_1.png">
+                    <span class="pacifico">Luca</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Luisa_1.png">
+                    <span class="pacifico">Luisa</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Manfredi_1.png">
+                    <span class="pacifico">Manfredi</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Marcaiello_1.png">
+                    <span class="pacifico">Marcaiello</span>
+                </figure>
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Marco_1.png">
+                    <span class="pacifico">Marco</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Marta_1.png">
+                    <span class="pacifico">Marta</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Martina_1.png">
+                    <span class="pacifico">Martina</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Matte G_1.png">
+                    <span class="pacifico">Matteo G</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Matteo U_1.png">
+                    <span class="pacifico">Matteo U</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Noemi_1.png">
+                    <span class="pacifico">Noemi</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Sasha_1.png">
+                    <span class="pacifico">Sasha</span>
+                </figure>
+
+                <figure class="animatori-item">
+                    <img class="" src="<?php echo get_template_directory_uri(); ?>/img/animatori/Susanna_1.png">
+                    <span class="pacifico">Susanna</span>
+                </figure>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- //Wrapper Animatori -->
+
+<?php get_footer(); ?>
--- wp-content/themes/understrap/index.php
+++ wp-content/themes/understrap/index.php
@@ -10,307 +10,63 @@
  *
  * @package understrap
  */
-
 get_header();
-
 $container   = get_theme_mod( 'understrap_container_type' );
-$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 ?>
 
 <?php if ( is_front_page() && is_home() ) : ?>
 	<?php get_template_part( 'global-templates/hero' ); ?>
 <?php endif; ?>
 
-<!-- Wrapper Hero -->
-<section class="wrapper hero-wrapper" id="wrapper-hero">
-	<div class="owl-carousel hero-carousel">
+<div class="wrapper" id="wrapper-index">
 
-		<div class="hero-carousel--item" style="background-image:url(<?php echo get_template_directory_uri(); ?>/img/home_light.jpg);">
-			<div class="hero-carousel--item--info">
-				<span class="titolo giallo pacifico">Campi Estivi</span>
-				<h3 class="bianco f-bold">Campo Superiori 2016 <br>Il Re Leone</h3>
-				<a href="" class="btn">Scopri</a>
-			</div>
-		</div>
-
-		<div class="hero-carousel--item" style="background-image:url(<?php echo get_template_directory_uri(); ?>/img/home_light.jpg);">
-			<div class="hero-carousel--item--info">
-				<span class="titolo giallo">Campi Estivi</span>
-				<h3 class="bianco">Campo Medie 2016 <br>Il Re Leone</h3>
-				<a href="" class="btn">Scopri</a>
-			</div>
-		</div>
-	</div>
-
-
-</section><!-- //Wrapper Hero -->
-
-<!-- Wrapper Trio -->
-<section class="wrapper wrapper-trio after-hero" id="wrapper-trio">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-4 col-sm-12 trio-obj">
-				<i class="fa fa-cutlery trio-obj--icon" aria-hidden="true"></i>
-				<h3 class="pacifico">Cene e Feste</h3>
-				<p class="trio-obj--descr">
-				Organizziamo feste a tema e cene di raccolta fondi per finanziare le nostre attività o per sostenere progetti di altre Associazioni del territorio
-				</p>
-				<a href="" class="btn rosso bordo">Scopri</a>
-			</div>
-
-			<div class="col-md-4 col-sm-12 trio-obj">
-				<i class="fa fa-music trio-obj--icon" aria-hidden="true"></i>
-				<h3 class="pacifico">Animazione</h3>
-				<p class="trio-obj--descr">
-				Animiamo feste di compleanno per bambini con musica, giochi e truccabimbo in cambio di un’offerta a sostegno dei nostri progetti
-				</p>
-				<a href="" class="btn verde bordo">Scopri</a>
-			</div>
-
-			<div class="col-md-4 col-sm-12 trio-obj">
-				<i class="fa fa-comments trio-obj--icon" aria-hidden="true"></i>
-				<h3 class="pacifico">Incontri e Seminari</h3>
-				<p class="trio-obj--descr">
-				Creiamo momenti di incontro e discussione, gratuiti e aperti a tutti, su temi di interesse del Gruppo o su questioni di attualità
-				</p>
-				<a href="" class="btn bordo">Scopri</a>
-			</div>
-		</div>
-	</div>
-</section><!-- //Wrapper Trio -->
-
-<!-- Wrapper Info -->
-<section class="wrapper wrapper-info" id="wrapper-info">
-	<div class="container">
-		<div class="row">
-			<figure class="col-md-6">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/header.jpg" alt="Sangiorgello Gruppo Giovani">
-			</figure>
-			<div class="col-md-6 col-sm-12">
-				<span class="titolo giallo pacifico">Welcome to</span>
-				<h1 class="blu f-bold">Gruppo Giovani <br>Sangiorgello</h1>
-				<p>
-				Il Gruppo Giovani Sangiorgello è nato come gruppo informale di ragazzi e ragazze delle Parrocchie di Gello e San Giorgio con l’obiettivo di dare un contributo attivo alla vita della comunità parrocchiale.
-				</p>
-				<a href="" class="btn">Scopri di più</a>
-			</div>
-		</div>
-	</div>
-</section><!-- //Wrapper Info -->
-
-<!-- Wrapper Stats -->
-<section class="wrapper" id="wrapper-stats">
-	<figure class="c-c-combo-breaker"></figure>
-</section><!-- //Wrapper Stats -->
-<!-- Wrapper Activities -->
-<section class="wrapper" id="wrapper-activities">
-	<div class="container">
-		<div class="row">
-			<div class="wrapper-title">
-				<span class="pre-titolo f-bold">Estate</span>
-				<h3 class="titolo rosso pacifico">Campi estivi</h3>
-			</div>
-		</div>
-
-		<div class="row cards-wrapper">
-			<article class="card">
-				<figure style="background-image:url(<?php echo get_template_directory_uri(); ?>/img/Campo_Elementari.png);"></figure>
-				<header>
-					<h2 class="blu pacifico">Campo Elementari</h2>
-				</header>
-				<p>
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-					Suscipit, nostrum quidem? Commodi doloremque ad quam.
-				</p>
-				<div class="card--footer">
-					<span class="blu f-bold">7-11 anni</span>
-					<a href="" class="f-bold">Iscriviti</a>
-				</div>
-			</article>
-			<article class="card">
-				<figure style="background-image:url(<?php echo get_template_directory_uri(); ?>/img/Campo_Medie.png);"></figure>
-				<header>
-					<h2 class="verde pacifico">Campo Medie</h2>
-				</header>
-				<p>
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-					Suscipit, nostrum quidem? Commodi doloremque ad quam.
-				</p>
-				<div class="card--footer">
-					<span class="verde f-bold">7-11 anni</span>
-					<a href="" class="f-bold">Iscriviti</a>
-				</div>
-			</article>
-			<article class="card">
-				<figure style="background-image:url(<?php echo get_template_directory_uri(); ?>/img/Campo_Superiori.png);"></figure>
-				<header>
-					<h2 class="rosso pacifico">Campo Superiori</h2>
-				</header>
-				<p>
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-					Suscipit, nostrum quidem? Commodi doloremque ad quam.
-				</p>
-				<div class="card--footer">
-					<span class="rosso f-bold">7-11 anni</span>
-					<a href="" class="f-bold">Iscriviti</a>
-				</div>
-			</article>
-		</div>
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
 		<div class="row">
-			<a class="btn margin-o-auto rosso" href="">Tutti i campi</a>
-		</div>
-	</div>
-</section><!-- //Wrapper Activities -->
 
-<!-- Wrapper Faces -->
-<section class="wrapper bg-giallo" id="wrapper-faces">
-	<div class="container">
-		<div class="row">
-			<div class="wrapper-title">
-				<span class="pre-titolo f-bold">Animatori</span>
-				<h3 class="rosso pacifico">Scopriamo i volti</h3>
-			</div>
-		</div>
-		<div class="owl-carousel scopri-carousel">
-			<div class="face-wrapper">
-				<figure class="face-wrapper--pic">
-					<img class="" src="<?php echo get_template_directory_uri(); ?>/img/Animatore_jonny.jpg" alt="First slide">
-				</figure>
-				
-				<div class="face-wrapper--descr">
-					<h5 class="pacifico f-s-xl blu">Jonny</h5>
-					<p class="f-bold">25 anni</p>
-				</div>
-			</div>	
+			<!-- Do the left sidebar check and opens the primary div -->
+			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
 
-			<div class="face-wrapper">
-				<figure class="face-wrapper--pic">
-					<img class="" src="<?php echo get_template_directory_uri(); ?>/img/Animatore_danda.jpg" alt="First slide">
-				</figure>
-				
-				<div class="face-wrapper--descr">
-					<h5 class="pacifico f-s-xl blu">Amanda</h5>
-					<p class="f-bold">27 anni</p>
-				</div>
-			</div>
+			<main class="site-main" id="main">
 
-			<div class="face-wrapper">
-				<figure class="face-wrapper--pic">
-					<img class="" src="<?php echo get_template_directory_uri(); ?>/img/Animatore_jonny.jpg" alt="First slide">
-				</figure>
-				
-				<div class="face-wrapper--descr">
-					<h5 class="pacifico f-s-xl blu">Jonny</h5>
-					<p class="f-bold">25 anni</p>
-				</div>
-			</div>
+				<?php if ( have_posts() ) : ?>
 
-			<div class="face-wrapper">
-				<figure class="face-wrapper--pic">
-					<img class="" src="<?php echo get_template_directory_uri(); ?>/img/Animatore_danda.jpg" alt="First slide">
-				</figure>
-				
-				<div class="face-wrapper--descr">
-					<h5 class="pacifico f-s-xl blu">Amanda</h5>
-					<p class="f-bold">27 anni</p>
-				</div>
-			</div>
+					<?php /* Start the Loop */ ?>
 
-		</div>
+					<?php while ( have_posts() ) : the_post(); ?>
 
-		<div class="row">
-			<a href="" class="btn margin-o-auto rosso bordo">Scopri tutti</a>
-		</div>
+						<?php
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'loop-templates/content', get_post_format() );
+						?>
+
+					<?php endwhile; ?>
+
+				<?php else : ?>
+
+					<?php get_template_part( 'loop-templates/content', 'none' ); ?>
+
+				<?php endif; ?>
+
+			</main><!-- #main -->
+
+			<!-- The pagination component -->
+			<?php understrap_pagination(); ?>
+
+		</div><!-- #primary -->
+
+		<!-- Do the right sidebar check -->
+		<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
 		
-	</div>
-	
-</section><!-- //Wrapper Faces -->
 
-<!-- Wrapper Gallery -->
-<section class="wrapper" id="wrapper-faces">
-	<div class="container">
-		<div class="row">
-			<div class="wrapper-title">
-				<span class="pre-titolo f-bold">Galleria</span>
-				<h3 class="verde pacifico">I nostri momenti</h3>
-			</div>
-		</div>
+	</div><!-- .row -->
 
-		<div class="owl-carousel galleria-carousel">
-		
-		<?php 
-		for($i=0;$i<9;$i++) {
-			?>
-			<figure class="galleria-item">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/header.jpg" alt="">
-			</figure>
-			<?php
-		}
-		?>
-		</div>
-	</div>
-</section><!-- //Wrapper Gallery -->
+</div><!-- Container end -->
 
-<!-- Wrapper Events -->
-<section class="wrapper bg-rosso" id="wrapper-events">
-	<div class="container">
-		<div class="row">
-			<div class="wrapper-title">
-				<span class="pre-titolo f-bold bianco">Eventi</span>
-				<h3 class="pacifico giallo">Prossimi Appuntamenti</h3>
-			</div>
-		</div>
+</div><!-- Wrapper end -->
 
-		<div class="row cards-wrapper">
-			<article class="card col-md-4 col-sm-12">
-				<figure style="background-image:url(<?php echo get_template_directory_uri(); ?>/img/header.jpg);"></figure>
-
-				<header>
-					<h2 class="blu pacifico">Festa Carnevale</h2>
-					<span class="giallo f-bold">26 feb</span>
-				</header>
-				
-				<p>
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-					Suscipit, nostrum quidem? Commodi doloremque ad quam.
-				</p>
-				<div class="card--footer">
-					<span class="blu f-bold">15:00</span>
-					<span class="giallo f-bold">Oggi</span>
-				</div>
-			</article>
-			<article class="card col-md-4 col-sm-12">
-				<figure style="background-image:url(<?php echo get_template_directory_uri(); ?>/img/header.jpg);"></figure>
-				<header>
-					<h2 class="blu pacifico">Festa Carnevale</h2>
-					<span class="giallo f-bold">26 feb</span>
-				</header>
-				<p>
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-					Suscipit, nostrum quidem? Commodi doloremque ad quam.
-				</p>
-				<div class="card--footer">
-					<span class="blu f-bold">15:00</span>
-					<span class="giallo f-bold">Oggi</span>
-				</div>
-			</article>
-			<article class="card col-md-4 col-sm-12">
-				<figure style="background-image:url(<?php echo get_template_directory_uri(); ?>/img/header.jpg);"></figure>
-				<header>
-					<h2 class="blu pacifico">Festa Carnevale</h2>
-					<span class="giallo f-bold">26 feb</span>
-				</header>
-				<p>
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-					Suscipit, nostrum quidem? Commodi doloremque ad quam.
-				</p>
-				<div class="card--footer">
-					<span class="blu f-bold">15:00</span>
-					<span class="giallo f-bold">Oggi</span>
-				</div>
-			</article>
-		</div>
-	</div>
-</section><!-- //Wrapper Events -->
 <?php get_footer(); ?>
