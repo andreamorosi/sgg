f35f5272202c2495c2c9e126adc742692c91b7af
andreamorosi <andreamorosi94@gmail.com>
2018-01-30 21:58:36 +0100

    Chi Siamo page, missing downloads

---
+++ wp-content/themes/understrap/chisiamo.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Template Name: Chi Siamo
+ *
+ * @package understrap
+ */
+
+get_header();
+?>
+
+<section class="wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="wrapper-title">
+                    <span class="pre-titolo f-bold">SGG</span>
+                    <h2 class="blu pacifico">Sangiorgello Gruppo Giovanni</h2>
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-md-6">
+                <div class="row margin-v-2">
+                    <h3 class="giallo pacifico">Chi siamo</h3>
+                    <p class="text-align-right">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum nulla odit doloribus rerum eius repellendus necessitatibus tenetur in, illum iste voluptatem quaerat. Tempora impedit consequuntur esse! Velit ducimus officia totam.
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-md-6 offset-md-6">
+                <div class="row margin-v-2">
+                    <h3 class="giallo pacifico">Cosa facciamo</h3>
+                    <p class="text-align-left">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum nulla odit doloribus rerum eius repellendus necessitatibus tenetur in, illum iste voluptatem quaerat. Tempora impedit consequuntur esse! Velit ducimus officia totam.
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-md-6">
+                <div class="row margin-v-2">
+                    <h3 class="giallo pacifico">Chi siamo</h3>
+                    <p class="text-align-right">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum nulla odit doloribus rerum eius repellendus necessitatibus tenetur in, illum iste voluptatem quaerat. Tempora impedit consequuntur esse! Velit ducimus officia totam.
+                    </p>
+                </div>
+            </div>
+
+            
+        </div>
+    </div>
+</section>
+
+<section class="wrapper bg-verde">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="wrapper-title">
+                    <span class="pre-titolo f-bold bianco">Area Download</span>
+                    <h2 class="giallo pacifico">Download</h2>
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-md-4">
+
+            </div>
+            <div class="col-sm-12 col-md-4">
+                
+            </div>
+            <div class="col-sm-12 col-md-4">
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
