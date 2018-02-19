10f675adfb7e88ac37516c4b3a83c5081362c0ea
andreamorosi <andreamorosi94@gmail.com>
2018-02-19 13:43:12 +0100

    Campi estivi page

---
+++ wp-content/themes/understrap/campiestivi.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * Template Name: Campi Estivi
+ *
+ * @package understrap
+ */
+
+get_header();
+?>
+
+<!-- Wrapper Campi Estivi -->
+<section class="wrapper campi-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="wrapper-title">
+                    <span class="pre-titolo f-bold">Campi Estivi</span>
+                    <h3 class="titolo rosso pacifico">Sangiorgello Gruppo Giovani</h3>
+                </div>
+            </div>
+
+            <div class="col-sm-12 offset-sm-0 col-md-8 offset-md-2 text-center">
+
+                <p class="margin-v-2">
+                Il campo estivo è un soggiorno residenziale di una settimana con finalità principalmente educative ma anche ricreative, gestito interamente dagli animatori volontari.
+Durante il campo estivo i ragazzi vivono insieme tutti i momenti della giornata, che risulta solitamente divisa in un’attività di catechesi mattutina e attività ludiche e laboratoriali nelle ore pomeridiane e serali. La settimana di campo è arricchita da una specifica ambientazione, predisposta in base alla catechesi e ai messaggi educativi scelti. Il campo è rivolto ai ragazzi compresi nei gruppi di catechismo, suddivisi in fasce d’età, ma è aperto anche a coetanei di realtà diverse e non facenti parte dei gruppi parrocchiali.
+                </p>
+            </div>
+
+            <div class="col-12 campi-inner">
+                    <div class="campi-card">
+                        <div class="campi-card--header">
+                            <h4 class="pacifico">Campo Elementari</h4>
+                            <h5>Bambini dai 7 agli 11 anni</h5>
+                        </div>
+                        <ul class="campi-card--main">
+                            <li class="campi-card--info">
+                                <span>Giorni</span>
+                                <p>
+                                    24-30 Luglio 2017
+                                </p>
+                            </li>
+                            <li class="campi-card--info">
+                                <span>Località</span>
+                                <p>
+                                    Cavallico
+                                </p>
+                            </li>
+                            <li class="campi-card--info">
+                                <span>info3</span>
+                                <p>
+                                    Info 3
+                                </p>
+                            </li>
+                            <li class="campi-card--info">
+                                <span>info4</span>
+                                <p>
+                                    Info 4
+                                </p>
+                            </li>
+                        </ul>
+                        <div class="campi-card--footer">
+                            <p>Altre Info</p>
+                            <p class="campi-animatore">Susanna 333333333</p>
+                            <a href="" class="btn">Scrivici</a>
+                        </div>
+                </div>
+
+
+                    <div class="campi-card">
+                        <div class="campi-card--header">
+                            <h4 class="pacifico">Campo Medie</h4>
+                            <h5>Bambini dai 7 agli 11 anni</h5>
+                        </div>
+                        <ul class="campi-card--main">
+                            <li class="campi-card--info">
+                                <span>Giorni</span>
+                                <p>
+                                    24-30 Luglio 2017
+                                </p>
+                            </li>
+                            <li class="campi-card--info">
+                                <span>Località</span>
+                                <p>
+                                    Cavallico
+                                </p>
+                            </li>
+                            <li class="campi-card--info">
+                                <span>info3</span>
+                                <p>
+                                    Info 3
+                                </p>
+                            </li>
+                            <li class="campi-card--info">
+                                <span>info4</span>
+                                <p>
+                                    Info 4
+                                </p>
+                            </li>
+                        </ul>
+                        <div class="campi-card--footer">
+                            <p>Altre Info</p>
+                            <p class="campi-animatore">Susanna 333333333</p>
+                            <a href="" class="btn verde">Scrivici</a>
+                        </div>
+                </div>
+
+
+                    <div class="campi-card">
+                        <div class="campi-card--header">
+                            <h4 class="pacifico">Campo Superiori</h4>
+                            <h5>Bambini dai 7 agli 11 anni</h5>
+                        </div>
+                        <ul class="campi-card--main">
+                            <li class="campi-card--info">
+                                <span>Giorni</span>
+                                <p>
+                                    24-30 Luglio 2017
+                                </p>
+                            </li>
+                            <li class="campi-card--info">
+                                <span>Località</span>
+                                <p>
+                                    Cavallico
+                                </p>
+                            </li>
+                            <li class="campi-card--info">
+                                <span>info3</span>
+                                <p>
+                                    Info 3
+                                </p>
+                            </li>
+                            <li class="campi-card--info">
+                                <span>info4</span>
+                                <p>
+                                    Info 4
+                                </p>
+                            </li>
+                        </ul>
+                        <div class="campi-card--footer">
+                            <p>Altre Info</p>
+                            <p class="campi-animatore">Susanna 333333333</p>
+                            <a href="" class="btn rosso">Scrivici</a>
+                        </div>
+                </div>
+            </div>
+
+            
+        </div>
+    </div>
+    
+</section>
+<!-- //Wrapper Animatori -->
+
+<section class="wrapper bg-rosso">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="wrapper-title">
+                    <span class="pre-titolo f-bold bianco">Area Download</span>
+                    <h2 class="giallo pacifico">Download</h2>
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-md-4">
+
+            </div>
+            <div class="col-sm-12 col-md-4">
+                
+            </div>
+            <div class="col-sm-12 col-md-4">
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
